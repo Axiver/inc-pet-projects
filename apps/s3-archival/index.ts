818d242892da44e4aceac0c1fede298a9ea122c8
George Yin Xu <georgeyinxu@gmail.com>
2023-06-03 21:57:00 +0800

    added archival functionalities

--- apps/s3-archival/index.ts
+++ apps/s3-archival/index.ts
@@ -1,7 +1,17 @@
 import { advertHandler, listingsHandler, roomsHandler } from "./handlers";
+import { Glacier } from "aws-sdk";
+import { Readable } from "stream";
+import * as fs from "fs";
+import * as path from "path";
 
 // -- Constants -- //
 const archivalDate = new Date(Date.now() - 1000 * 60 * 60 * 24); // 3 Months
+const glacier = new Glacier({ region: process.env.AWS_REGION });
+const vaultName = "siwma-archival";
+const projectDir = path.resolve(__dirname);
+const csvFilePath = path.join(projectDir, "output", "test.csv");
+const archiveId =
+  "KjrYazB2aEYSxrqp0mrxonO12GsMCHfQRG0ugE7MnS6m25BfP99C58iLFOnaZa4Q0zF_Zzw-IsKj4BeM0Tv9eculD0yk6Ci4go9IgWQqnopUhtQi8sy43enflFk9WyxQU45fy9i97w";
 
 // -- Functions -- //
 /**
@@ -41,13 +51,19 @@ const identifyDataToBeArchived = async () => {
 /**
  * Obtains the data to be archived
  */
-const getDataToBeArchived = async (dataToBeArchived: Awaited<ReturnType<typeof identifyDataToBeArchived>>) => {
+const getDataToBeArchived = async (
+  dataToBeArchived: Awaited<ReturnType<typeof identifyDataToBeArchived>>
+) => {
   // Construct array of ids to be archived
   const listingIds = dataToBeArchived.listings.map((listing) => listing.id);
-  const bookmarkIds = dataToBeArchived.listings.map((listing) => listing.listingBookmarks.map((bookmark) => bookmark.id)).flat();
+  const bookmarkIds = dataToBeArchived.listings
+    .map((listing) => listing.listingBookmarks.map((bookmark) => bookmark.id))
+    .flat();
   const advertIds = dataToBeArchived.advertisements.map((advert) => advert.id);
   const roomIds = dataToBeArchived.rooms.map((room) => room.id);
-  const messageIds = dataToBeArchived.rooms.map((room) => room.messages.map((message) => message.id)).flat();
+  const messageIds = dataToBeArchived.rooms
+    .map((room) => room.messages.map((message) => message.id))
+    .flat();
 
   // Retrieve the data to archive
   const listings = await listingsHandler.get(listingIds);
@@ -65,10 +81,40 @@ const getDataToBeArchived = async (dataToBeArchived: Awaited<ReturnType<typeof i
   };
 };
 
+const uploadCSVToGlacier = async (vaultName: string, filePath: string) => {
+  const fileData = fs.readFileSync(filePath);
+  const fileBuffer = Buffer.from(fileData);
+
+  const uploadResponse = await glacier
+    .uploadArchive({
+      accountId: process.env.AWS_ACCOUNT_ID,
+      vaultName,
+      archiveDescription: `CSV file: ${filePath}`,
+      body: fileBuffer,
+    })
+    .promise();
+
+  console.log(`Upload successful. Archive ID: ${uploadResponse.archiveId}`);
+};
+
+const retrieveFromGlacier = async (): Promise<any> => {
+  glacier.initiateJob({
+    accountId: process.env.AWS_ACCOUNT_ID,
+    vaultName,
+    jobParameters: {
+      Type: "archive-retrieval",
+      ArchiveId: archiveId,
+      Description: "Archive retrieval job",
+    },
+  }).promise();
+};
+
 // -- Main -- //
 const main = async () => {
   // Print out some information about the archival process
-  console.log("Starting archival process for database " + process.env.DATABASE_URL);
+  console.log(
+    "Starting archival process for database " + process.env.DATABASE_URL
+  );
   console.log(`Archiving listings older than ${archivalDate}`);
   console.log("Identifying data to archive");
 
@@ -83,7 +129,59 @@ const main = async () => {
   console.log("Data retrieved");
   console.log("Generating csv files");
 
-  console.log({ data });
+  console.log("CSV Uploaded");
+  console.log("Uploading to Glacier");
+
+  // uploadCSVToGlacier(vaultName, csvFilePath)
+  //   .then(() => {
+  //     console.log("All files uploaded to Glacier successfully");
+  //   })
+  //   .catch((error) => {
+  //     console.error("Failed: ", error);
+  //   });
+
+  // console.log({ data });
+
+  // Retrieving of Archive (By initiating through jobs)
+  // glacier.initiateJob(
+  //   {
+  //     accountId: '-',
+  //     vaultName: vaultName,
+  //     jobParameters: {
+  //       Type: "archive-retrieval",
+  //       ArchiveId: archiveId,
+  //       Description: "Archive retrieval job",
+  //     },
+  //   },
+  //   function (err, data) {
+  //     if (err) {
+  //       console.log('Error initiating retrieval job:', err);
+  //     } else {
+  //       console.log('Retrieval job initiated successfully.');
+  //       console.log('Job ID:', data.jobId);
+  //     }
+  //   }
+  // );
+
+  // Getting job progress
+  // glacier.describeJob(
+  //   {
+  //     accountId: '-',
+  //     vaultName: vaultName,
+  //     jobId: "VjFSF5MgORJyKs8qPurGiZo0jRQqpByRguZEDoY8f4B9XVzX6vOzKV8B0s3jLfKPBH87tNon24mEnhzeIQzg9ycB6ZFU",
+  //   },
+  //   function (err, data) {
+  //     if (err) {
+  //       console.log('Error retrieving job status:', err);
+  //     } else {
+  //       console.log('Job status:', data.StatusCode);
+  //       console.log('Job completion date:', data.CompletionDate);
+  //       console.log('Job output location:', data.SNSTopic);
+  //       console.log('Job archive size:', data.ArchiveSizeInBytes);
+  //       // ... handle other job properties as needed
+  //     }
+  //   }
+  // );
 };
 
 main();
