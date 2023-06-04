8db758d91dc9bdd4ab1acf8c3603fd14aa32950d
George Yin Xu <georgeyinxu@gmail.com>
2023-06-04 20:53:17 +0800

    got the upload functionality and updating of json done

--- apps/s3-archival/index.ts
+++ apps/s3-archival/index.ts
@@ -9,9 +9,8 @@ const archivalDate = new Date(Date.now() - 1000 * 60 * 60 * 24); // 3 Months
 const glacier = new Glacier({ region: process.env.AWS_REGION });
 const vaultName = "siwma-archival";
 const projectDir = path.resolve(__dirname);
-const csvFilePath = path.join(projectDir, "output", "test.csv");
-const archiveId =
-  "KjrYazB2aEYSxrqp0mrxonO12GsMCHfQRG0ugE7MnS6m25BfP99C58iLFOnaZa4Q0zF_Zzw-IsKj4BeM0Tv9eculD0yk6Ci4go9IgWQqnopUhtQi8sy43enflFk9WyxQU45fy9i97w";
+const currDate = new Date().toISOString().split("T")[0];
+const csvFilePath = path.join(projectDir, "output", currDate);
 
 // -- Functions -- //
 /**
@@ -51,13 +50,19 @@ const identifyDataToBeArchived = async () => {
 /**
  * Obtains the data to be archived
  */
-const getDataToBeArchived = async (dataToBeArchived: Awaited<ReturnType<typeof identifyDataToBeArchived>>) => {
+const getDataToBeArchived = async (
+  dataToBeArchived: Awaited<ReturnType<typeof identifyDataToBeArchived>>
+) => {
   // Construct array of ids to be archived
   const listingIds = dataToBeArchived.listings.map((listing) => listing.id);
-  const bookmarkIds = dataToBeArchived.listings.map((listing) => listing.listingBookmarks.map((bookmark) => bookmark.id)).flat();
+  const bookmarkIds = dataToBeArchived.listings
+    .map((listing) => listing.listingBookmarks.map((bookmark) => bookmark.id))
+    .flat();
   const advertIds = dataToBeArchived.advertisements.map((advert) => advert.id);
   const roomIds = dataToBeArchived.rooms.map((room) => room.id);
-  const messageIds = dataToBeArchived.rooms.map((room) => room.messages.map((message) => message.id)).flat();
+  const messageIds = dataToBeArchived.rooms
+    .map((room) => room.messages.map((message) => message.id))
+    .flat();
 
   // Retrieve the data to archive
   const listings = await listingsHandler.get(listingIds);
@@ -75,45 +80,97 @@ const getDataToBeArchived = async (dataToBeArchived: Awaited<ReturnType<typeof i
   };
 };
 
-const uploadCSVToGlacier = async (vaultName: string, filePath: string) => {
-  const fileData = fs.readFileSync(filePath);
-  const fileBuffer = Buffer.from(fileData);
+const uploadCSVToGlacier = async (vaultName: string, filePath: string, dataToBeArchived) => {
+  const jsonPath = path.join(projectDir, 'output', 'index.json')
+  const jsonData = fs.readFileSync(jsonPath, 'utf-8')
+  const parsedJsonData = JSON.parse(jsonData)
 
-  const uploadResponse = await glacier
-    .uploadArchive({
-      accountId: process.env.AWS_ACCOUNT_ID,
-      vaultName,
-      archiveDescription: `CSV file: ${filePath}`,
-      body: fileBuffer,
-    })
-    .promise();
+  fs.readdir(filePath, (err, files) => {
+    if (err) {
+      console.log("Error reading directory:", err);
+      return;
+    }
 
-  console.log(`Upload successful. Archive ID: ${uploadResponse.archiveId}`);
+    const fileNames = files.filter((file) => {
+      return fs.statSync(`${filePath}/${file}`).isFile();
+    });
+
+    fileNames.forEach(async (fileName) => {
+      const csvPath = path.join(filePath, fileName);
+      const type = fileName.split('.')[0]
+      const fileData = fs.readFileSync(csvPath);
+      const fileBuffer = Buffer.from(fileData);
+      let ids: number[] = []
+
+      const uploadResponse = await glacier
+        .uploadArchive({
+          accountId: process.env.AWS_ACCOUNT_ID,
+          vaultName,
+          archiveDescription: `CSV file: ${filePath}`,
+          body: fileBuffer,
+        })
+        .promise();
+
+      console.log(`Upload successful. Archive ID: ${uploadResponse.archiveId}`);
+
+      if (type === 'advertisements') {
+        ids = dataToBeArchived.advertisements.map((advertisement) => advertisement.id)
+      } else if (type === 'listingBookmarks') {
+        dataToBeArchived.listings.forEach((listing) => {
+          listing.listingBookmarks.forEach((bookmark) => {
+            ids.push(bookmark.id)
+          })
+        })
+      } else if (type === 'listings') {
+        ids = dataToBeArchived.listings.map((listing) => listing.id)
+      } else if (type === 'messages') {
+        dataToBeArchived.rooms.forEach((room) => {
+          room.messages.forEach((message) => {
+            ids.push(message.id)
+          })
+        })
+      } else if (type === 'rooms') {
+        ids = dataToBeArchived.rooms.map((room) => room.id)
+      }
+
+      ids.forEach(id => {
+        if (!parsedJsonData[type]) {
+          parsedJsonData[type] = {}; // Initialize the object if it doesn't exist
+        }
+        parsedJsonData[type][id] = uploadResponse.archiveId;
+      })
+
+      fs.writeFileSync(jsonPath, JSON.stringify(parsedJsonData, null, 2))
+    });
+  });
 };
 
-const retrieveFromGlacier = async (): Promise<any> => {
-  glacier
-    .initiateJob({
-      accountId: process.env.AWS_ACCOUNT_ID,
-      vaultName,
-      jobParameters: {
-        Type: "archive-retrieval",
-        ArchiveId: archiveId,
-        Description: "Archive retrieval job",
-      },
-    })
-    .promise();
-};
+// const retrieveFromGlacier = async (): Promise<any> => {
+//   glacier
+//     .initiateJob({
+//       accountId: process.env.AWS_ACCOUNT_ID,
+//       vaultName,
+//       jobParameters: {
+//         Type: "archive-retrieval",
+//         ArchiveId: archiveId,
+//         Description: "Archive retrieval job",
+//       },
+//     })
+//     .promise();
+// };
 
 // -- Main -- //
 const main = async () => {
   // Print out some information about the archival process
-  console.log("Starting archival process for database " + process.env.DATABASE_URL);
+  console.log(
+    "Starting archival process for database " + process.env.DATABASE_URL
+  );
   console.log(`Archiving listings older than ${archivalDate}`);
   console.log("Identifying data to archive");
 
   // Identify the data to be archived
   const dataToBeArchived = await identifyDataToBeArchived();
+  console.log(dataToBeArchived)
 
   console.log("Retrieving data to archive");
 
@@ -124,27 +181,26 @@ const main = async () => {
   console.log("Generating CSVs");
 
   // Create folder for CSVs
-  const currDate = new Date().toISOString().split("T")[0];
   const outputDir = path.join("./output/", currDate);
   await createOutputFolder(outputDir);
 
   // Generate CSV files
-  const promises = Object.keys(data).map((key) => generateCSV(outputDir, data[key], key));
+  const promises = Object.keys(data).map((key) =>
+    generateCSV(outputDir, data[key], key)
+  );
 
   await Promise.all(promises);
 
   console.log("CSV Uploaded");
   console.log("Uploading to Glacier");
 
-  // uploadCSVToGlacier(vaultName, csvFilePath)
-  //   .then(() => {
-  //     console.log("All files uploaded to Glacier successfully");
-  //   })
-  //   .catch((error) => {
-  //     console.error("Failed: ", error);
-  //   });
-
-  // console.log({ data });
+  uploadCSVToGlacier(vaultName, csvFilePath, dataToBeArchived)
+    .then(() => {
+      console.log("All files uploaded to Glacier successfully");
+    })
+    .catch((error) => {
+      console.error("Failed: ", error);
+    });
 
   // Retrieving of Archive (By initiating through jobs)
   // glacier.initiateJob(
